8e3ff9a4fad50a7fca4c9744e17ddada5a505415
yuanmeng <3040145436@qq.com>
2025-11-05 23:21:32 +0800

    2

--- practice/test1.js
+++ practice/test1.js
@@ -1,19 +1,17 @@
-
-
-let arr = [4,1,6,9,3,2,8,7]
-
-function quickSort(arr){
-  if(arr == null || arr.length == 0) return []
-  let leader =arr[0]
-  let left = []
-  let right = []
-  for(let i = 1;i < arr.length;i++){
-    if(arr[i] < leader) left.push(arr[i])
-      else right.push(arr[i])
+function Queue() {
+  this.arr = []
+  this.push = function(value){
+    this.arr.push(value)
   }
-  left = quickSort(left)
-  right = quickSort(right)
-  left.push(leader)
-  return left.concat(right)
+  this.shift = function(value){
+    this.arr.shift(value)
+  }
 }
-console.log(quickSort(arr));
+
+let queue = new Queue()
+queue.push(1)
+queue.push(2)
+queue.push(3)
+console.log(queue.arr);
+queue.shift()
+console.log(queue.arr);
